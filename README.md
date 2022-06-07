d2c521095c08f454765808e42f351f0fc545c535
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-06-07 10:28:59 +0200

    refactored card component and updated readme

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # ActivityLog-Component-CLASS-COMPONENT-REACT
 
+## Get Started
+
+Clone this repo
+
+```
+npm install
+```
+
+```
+npm start
+```
+
+## About
+
+Edit the user object in Data.js to view how the component handles those changes.
+
 ## Live-link
 
 https://activitylog-class-component-react.netlify.app/
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,23 +2,51 @@ import React, { Component } from 'react';
 import { ArrowCircleRightIcon } from '@heroicons/react/solid';
 
 export default class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showLog: false,
-      dynamicUserList: this.props.cardInfo.users,
-    };
+  state = {
+    showLog: false,
+    dynamicUserList: this.props.cardInfo.users,
+  };
+
+  toggleShowLog = () => {
+    !this.state.showLog
+      ? this.setState({ showLog: true })
+      : this.setState({ showLog: false });
+  };
+
+  renderUserSections(sections) {
+    return sections.map((user) => {
+      return (
+        <div
+          className='flex flex-col sm:flex-row gap-1 py-[0.2rem]'
+          key={user.commentId}>
+          <p className='text-sm font-black'>{user.name}:</p>
+          <p className='text-sm truncate'>{user.comment}</p>
+        </div>
+      );
+    });
   }
 
   render() {
-    const title = this.props.cardInfo.title;
-    const description = this.props.cardInfo.description;
-    const dynamicUserList = this.state.dynamicUserList;
+    const {
+      cardInfo: { title, description },
+    } = this.props;
+
+    const { dynamicUserList, showLog } = this.state;
 
     let uniqueUsers = [...new Set(dynamicUserList.map((user) => user.name))];
     let summaryString = '';
 
+    // - rendering different user sections depending on data
+    let sections = <p>No Data</p>;
+
+    if (dynamicUserList.length > 0) {
+      sections = showLog
+        ? this.renderUserSections(dynamicUserList)
+        : this.renderUserSections(dynamicUserList.slice(0, 3));
+    }
+
     //- creates the comment summary string depending on the amount of comments and users
+
     if (dynamicUserList.length > 0 && dynamicUserList.length < 3) {
       summaryString = 'No other comments';
     }
@@ -31,15 +59,7 @@ export default class Card extends Component {
       } others`;
     }
 
-    if (dynamicUserList.length < 0) {
-      summaryString = '';
-    }
-
-    const showLog = () => {
-      !this.state.showLog
-        ? this.setState({ showLog: true })
-        : this.setState({ showLog: false });
-    };
+    summaryString = !showLog ? summaryString : 'Viewing all comments';
 
     return (
       <article className='px-2'>
@@ -50,45 +70,12 @@ export default class Card extends Component {
           <p className='text-sm text-gray-400 mt-1'>{description}</p>
         </section>
         <div className='border-gray-100 border-2 py-2 px-3  mt-4 rounded-md'>
-          {dynamicUserList.length > 0 ? (
-            <section>
-              {this.state.showLog &&
-                dynamicUserList.map((user) => {
-                  return (
-                    <div
-                      className='flex flex-col sm:flex-row gap-1 py-[0.2rem]'
-                      key={user.commentId}>
-                      <p className='text-sm font-black'>{user.name}:</p>
-                      <p className='text-sm truncate'>{user.comment}</p>
-                    </div>
-                  );
-                })}
-              {!this.state.showLog &&
-                dynamicUserList.slice(0, 3).map((user) => {
-                  return (
-                    <div
-                      className='flex flex-col sm:flex-row gap-1 py-[0.2rem]'
-                      key={user.commentId}>
-                      <p className='text-sm font-black'>{user.name}:</p>
-                      <p className='text-sm truncate'>{user.comment}</p>
-                    </div>
-                  );
-                })}
-            </section>
-          ) : (
-            <section>
-              <p>No Data</p>
-            </section>
-          )}
+          <section>{sections}</section>
           <footer className='flex justify-between mt-2 text-xs'>
-            <p>
-              {!this.state.showLog ? summaryString : 'Viewing all comments'}
-            </p>
+            <p>{summaryString}</p>
             {dynamicUserList.length > 3 ? (
-              <button onClick={() => showLog()} className='font-bold'>
-                {!this.state.showLog
-                  ? 'View full activity log'
-                  : 'Collapse activity log'}
+              <button onClick={this.toggleShowLog} className='font-bold'>
+                {!showLog ? 'View full activity log' : 'Collapse activity log'}
                 {'  '}
                 <ArrowCircleRightIcon className='h-3 w-3 ml-1 inline-block' />
               </button>
